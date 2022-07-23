30b7fa547bde8091b8b764f938fbe7308bcf04a8
thienphu.dev97 <thienphu.dev97@gmail.com>
2022-07-23 16:35:19 +0700

    first commit

---
+++ src/ActiveUser.vue
@@ -0,0 +1,24 @@
+<template>
+  <h2>UserName: {{ name }}</h2>
+  <h3>Age: {{ age }}</h3>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style></style>
---
+++ src/UserData.vue
@@ -0,0 +1,36 @@
+<template>
+  <form @submit.prevent="submitData">
+    <div>
+      <label>UserName</label>
+      <input type="text" v-model="enterName" />
+    </div>
+
+    <div>
+      <label>Age</label>
+      <input type="number" v-model="enterAge" />
+    </div>
+
+    <div>
+      <button>Add User</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  emits: ["addUser"],
+  data() {
+    return {
+      enterName: "",
+      enterAge: "",
+    };
+  },
+  methods: {
+    submitData() {
+        this.$emit("addUser", this.enterName, this.enterAge);
+    },
+  },
+};
+</script>
+
+<style></style>
